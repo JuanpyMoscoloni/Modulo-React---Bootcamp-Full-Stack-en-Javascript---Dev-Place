0f4a3f156e3b8a1348081320c525d6d3348b23f9
JuanpyMoscoloni <moscolonijuanpablo@gmail.com>
2022-11-07 17:13:24 -0300

    PAIR REACT

--- Clase 16 React/src/App.js
+++ Clase 16 React/src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import './App.css';
+// import React from 'react';
+// import { Car } from './componentes/car';
+// import { Mostrar } from './componentes/car1';
+// import { Car2 } from './componentes/car2';
+
+
+
+
+
+// function App() {
+//   return (
+//     <>
+//     <Car />
+//     <Mostrar/>
+//     <Car2  />
+//     </>
+//   );
+// }
+
+// export default App;
+
+import  {Navbar}  from './componentes/navbar'; 
+import  {Footer}  from './componentes/footer';
+import  {Body}  from './componentes/body'; 
 import './App.css';
-import React from 'react';
-import { Car } from './componentes/car';
-import { Mostrar } from './componentes/car1';
-import { Car2 } from './componentes/car2';
-
-
-
-
 
 function App() {
-  return (
+ return (
     <>
-    <Car />
-    <Mostrar/>
-    <Car2  />
-    </>
-  );
+<Navbar />
+<Body />
+<Footer />
+
+
+</>
+ )
+  
 }
 
 export default App;
