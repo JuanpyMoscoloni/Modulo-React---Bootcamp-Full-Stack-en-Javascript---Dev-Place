b02793fd6150ac6f223f5e8d466504db094caede
JuanpyMoscoloni <moscolonijuanpablo@gmail.com>
2022-11-11 01:54:07 -0300

    11/11

---
+++ Clase 19 React/src/componentes/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+function Footer() {
+    return <footer className="page-footer font-small gray">
+
+        <div className="footer-copyright text-center py-3 ">© 2020 Copyright: 
+            <a href="https://github.com/JuanpyMoscoloni" className="ml-2 text-decoration-none">@JuanpyMoscoloni</a>
+        </div>
+
+    </footer>;
+}
+
+export default Footer;
